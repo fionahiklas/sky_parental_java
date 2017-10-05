bd6983d082a513e7651fe43e1e4d51ff2aa1b83a
Fiona Bianchi <fiona@hiklas.co.uk>
2017-10-05 14:58:59 +0100

    Implementing the interface

--- src/main/java/com/hiklas/mucking/around/SkyParentalControl.java
+++ src/main/java/com/hiklas/mucking/around/SkyParentalControl.java
@@ -1,7 +1,24 @@
 package com.hiklas.mucking.around;
 
-
-public class SkyParentalControl
+/**
+ * @author Fiona Bianchi
+ * @since 2017-10-05
+ */
+public class SkyParentalControl implements ParentalControlService
 {
 
+    @Override
+    public void setPreference(ParentalControlLevel level) {
+
+    }
+
+    @Override
+    public ParentalControlLevel getPreference() {
+        return null;
+    }
+
+    @Override
+    public boolean canWatch(String movieId) {
+        return false;
+    }
 }
